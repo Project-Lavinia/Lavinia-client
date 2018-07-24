01dde2cfa1fb0b07dac9fbf3e2833e83575b58c8
Simen <simen.d@gmail.com>
2018-07-25 01:05:10 +0200

    refactor: Remove useless comment

--- src/app/Layout/PresentationSettings/PresentationMenu.tsx
+++ src/app/Layout/PresentationSettings/PresentationMenu.tsx
@@ -4,7 +4,6 @@ import { default as PresentationSettings } from "./PresentationSettingsContainer
 
 export class PresentationMenu extends React.Component<{}, {}> {
     public render() {
-        // settings-menu contains the styling
         return (
             <div className="presentation-settings-container settings-menu">
                 <PresentationSelection />
