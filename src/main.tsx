c66930066f62efdbc44186b76f84d74b0168207a
Simen <8669774+simendag@users.noreply.github.com>
2018-07-26 12:11:04 +0200

    feat: Re-add loading and saving state

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
 import { createBrowserHistory } from "history";
-import { configureStore } from "./app/store";
+import { configureStore, loadState, saveState } from "./app/store";
 import { App } from "./app";
+import { throttle } from "lodash";
+
+const persistedState = loadState();
 
 const history = createBrowserHistory();
-const store = configureStore(history);
+const store = configureStore(history, persistedState);
+
+store.subscribe(
+    throttle(() => {
+        const currentState = store.getState();
+        if (currentState.requestedDataState.enableAutoSave) {
+            saveState(currentState);
+        }
+    }, 3000)
+);
 
 ReactDOM.render(
     <Provider store={store}>
