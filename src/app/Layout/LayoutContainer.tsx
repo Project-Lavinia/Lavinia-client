b0849fd605a2ff2446cb91ea3d3cf5987dca4172
Unknown <ajsivesind@gmail.com>
2018-07-29 13:22:52 +0200

    feat: Migrate to new API endpoint

--- src/app/Layout/LayoutContainer.tsx
+++ src/app/Layout/LayoutContainer.tsx
@@ -8,7 +8,7 @@ import { ElectionType } from "./Interfaces/Models";
 
 const mapDispatchToProps = (dispatch: any) => ({
     initializeState: async () => {
-        const uri = "https://mandater-testing.azurewebsites.net/api/v1.0.0/no/pe?deep=true";
+        const uri = "https://lavinia-api-staging.azurewebsites.net/api/v1.0.0/no/pe?deep=true";
         const failover: ElectionType = {
             internationalName: "UNDEFINED",
             electionTypeId: -1,
