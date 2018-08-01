d95e018102aae07ee8bbd4484b2c91034907095e
Simen <simen.d@gmail.com>
2018-08-02 00:49:22 +0200

    refactor: Type mapStateToProps

--- src/app/Layout/ComputationSettings/SettingMenuContainer.tsx
+++ src/app/Layout/ComputationSettings/SettingMenuContainer.tsx
@@ -4,10 +4,16 @@ import { SettingMenuComponent } from "./SettingMenuComponent";
 import { updateElectionData } from "../Computation";
 import { updateSettings, toggleAutoCompute } from ".";
 import { ComputationPayload, SettingsPayload } from "../Interfaces/Payloads";
-import { Election } from "../Interfaces/Models";
+import { Election, ElectionType } from "../Interfaces/Models";
 import { getAlgorithmType } from "../Logic/AlgorithmUtils";
 
-const mapStateToProps = (state: RootState) => ({
+interface PropsFromState {
+    computationPayload: ComputationPayload;
+    settingsPayload: SettingsPayload;
+    electionType: ElectionType;
+}
+
+const mapStateToProps = (state: RootState): PropsFromState => ({
     computationPayload: {
         election: state.computationState.election,
         algorithm: state.computationState.algorithm,
@@ -15,7 +21,7 @@ const mapStateToProps = (state: RootState) => ({
         electionThreshold: state.computationState.electionThreshold,
         districtSeats: state.computationState.districtSeats,
         levelingSeats: state.computationState.levelingSeats
-    } as ComputationPayload,
+    },
     settingsPayload: {
         electionYears: state.settingsState.electionYears,
         year: state.settingsState.year,
@@ -26,7 +32,7 @@ const mapStateToProps = (state: RootState) => ({
         levelingSeats: state.settingsState.levelingSeats,
         autoCompute: state.settingsState.autoCompute,
         forceCompute: false
-    } as SettingsPayload,
+    },
     electionType: state.requestedDataState.electionType
 });
 
