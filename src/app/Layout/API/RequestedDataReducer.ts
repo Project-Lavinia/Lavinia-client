b5e0bf283b2915ed912a6325dfe054efc38285e6
Simen <simen.d@gmail.com>
2018-08-01 22:42:14 +0200

    docs: Remove pointless documentation
    All our reducers already take this into account last I checked

--- src/app/Layout/API/RequestedDataReducer.ts
+++ src/app/Layout/API/RequestedDataReducer.ts
@@ -4,9 +4,6 @@ import { RequestedDataAction } from "../Types/ActionTypes";
 
 type KnownAction = InitializeRequestedDataAction;
 
-// NB: BaseReducer Typescript (Reducer<State>) definition changes as of redux 4.0.0
-// https://github.com/rt2zz/redux-persist/pull/778
-
 export function requestedDataAction(state: RequestedDataState | undefined, action: KnownAction): RequestedDataState {
     if (state === undefined) {
         state = unloadedState;
