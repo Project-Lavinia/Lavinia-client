64b4553474778a7bd22d08e13757ef68eedae4e4
Simen <8669774+simendag@users.noreply.github.com>
2018-10-02 06:31:31 +0200

    feat: add "all district" row

--- src/app/Layout/Presentation/Views/DistrictOverview.tsx
+++ src/app/Layout/Presentation/Views/DistrictOverview.tsx
@@ -8,36 +8,83 @@ export interface DistrictOverviewProps {
 
 export class DistrictOverview extends React.Component<DistrictOverviewProps, {}> {
     render() {
+        const data = this.props.districtResults;
         return (
             <ReactTable
                 className="-highlight -striped"
                 defaultPageSize={this.props.districtResults.length}
                 showPaginationBottom={false}
-                data={this.props.districtResults}
+                data={data}
                 columns={[
                     {
                         Header: "Fylke",
-                        accessor: "name"
+                        accessor: "name",
+                        Footer: (
+                            <span>
+                                <strong>Alle fylker</strong>
+                            </span>
+                        )
                     },
                     {
                         Header: "Stemmer",
-                        accessor: "votes"
+                        accessor: "votes",
+                        Footer: (
+                            <span>
+                                <strong>{data.map((value) => value.votes).reduce((acc, cur) => acc + cur, 0)}</strong>
+                            </span>
+                        )
                     },
                     {
                         Header: "Distrikt",
-                        accessor: "districtSeats"
+                        accessor: "districtSeats",
+                        Footer: (
+                            <span>
+                                <strong>
+                                    {data.map((value) => value.districtSeats).reduce((acc, cur) => acc + cur, 0)}
+                                </strong>
+                            </span>
+                        )
                     },
                     {
                         Header: "Utjevning",
-                        accessor: "levelingSeats"
+                        accessor: "levelingSeats",
+                        Footer: (
+                            <span>
+                                <strong>
+                                    {data.map((value) => value.levelingSeats).reduce((acc, cur) => acc + cur, 0)}
+                                </strong>
+                            </span>
+                        )
                     },
                     {
                         Header: "Sum",
-                        accessor: "totalSeats"
+                        accessor: "totalSeats",
+                        Footer: (
+                            <span>
+                                <strong>
+                                    {data.map((value) => value.totalSeats).reduce((acc, cur) => acc + cur, 0)}
+                                </strong>
+                                <br />
+                            </span>
+                        )
                     },
                     {
                         Header: "Stemmer/mandat",
-                        accessor: "votesPerSeat"
+                        accessor: "votesPerSeat",
+                        Footer: (
+                            <span>
+                                <strong>
+                                    {data.map((value) => value.votesPerSeat).reduce((acc, cur, ind, arr) => {
+                                        acc += cur;
+                                        if (ind === arr.length - 1) {
+                                            return acc / arr.length;
+                                        } else {
+                                            return acc;
+                                        }
+                                    })}
+                                </strong>
+                            </span>
+                        )
                     }
                 ]}
             />
