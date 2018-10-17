f5840be411817c1c5fd1775c0df86a78621c9261
Simen <simen.d@gmail.com>
2018-10-17 10:54:34 +0200

    docs: Fix typo in comment

--- src/app/Layout/Interfaces/Results/LevelingSeat.ts
+++ src/app/Layout/Interfaces/Results/LevelingSeat.ts
@@ -1,7 +1,7 @@
 export interface LevelingSeat {
     /** The number of when this seat was selected, 0 if it was never selected. */
     seatNumber: number;
-    /** The number this quotient had in the total quoteint list. */
+    /** The number this quotient had in the total quotient list. */
     quotientNumber: number;
     /** The district the seat belongs to */
     district: string;
