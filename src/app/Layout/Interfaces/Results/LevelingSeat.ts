e8f89b387d806da05186b46ca0948affcce46f70
Anders Jakob Sivesind <ajsivesind@gmail.com>
2018-10-17 02:40:33 +0200

    Implement storage of all quotients along with their index in the list
    Closes #36

--- src/app/Layout/Interfaces/Results/LevelingSeat.ts
+++ src/app/Layout/Interfaces/Results/LevelingSeat.ts
@@ -1,6 +1,8 @@
 export interface LevelingSeat {
     /** The number of when this seat was selected, 0 if it was never selected. */
     seatNumber: number;
+    /** The number this quotient had in the total quoteint list. */
+    quotientNumber: number;
     /** The district the seat belongs to */
     district: string;
     /** The party the seat belongs to */
