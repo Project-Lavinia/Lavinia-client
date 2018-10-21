6bd1cab04e04e07ee585bc616327b51e49e2da08
Simen <simen.d@gmail.com>
2018-10-21 14:19:31 +0200

    refactor: make skeleton component not crash

--- src/app/Layout/Presentation/Views/LevellingSeatOverview.tsx
+++ src/app/Layout/Presentation/Views/LevellingSeatOverview.tsx
@@ -8,11 +8,19 @@ interface LevellingSeatOverviewProps {
 }
 
 export class LevellingSeatOverview extends React.Component<LevellingSeatOverviewProps> {
+    makeData() {
+        return [];
+    }
+    getColumns(data: any) {
+        return [];
+    }
     render() {
+        const data = this.makeData();
+        const columns = this.getColumns(data);
         return (
             <React.Fragment>
-                <h2>Utjevningsmandater</h2>
-                <ReactTable />
+                <h3>Utjevningsmandater</h3>
+                <ReactTable data={data} columns={columns} />
             </React.Fragment>
         );
     }
