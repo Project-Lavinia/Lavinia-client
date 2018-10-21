59b2262dd5e2cd28e2dec958a599e9288609edcc
Simen <simen.d@gmail.com>
2018-10-21 13:46:21 +0200

    refactor: Reference new view from index-file for better imports

--- src/app/Layout/Presentation/Views/index.ts
+++ src/app/Layout/Presentation/Views/index.ts
@@ -3,3 +3,4 @@ export * from "./ElectionOverview";
 export * from "./SeatDistribution";
 export * from "./SeatsPerParty";
 export * from "./SingleDistrict";
+export * from "./LevellingSeatOverview";
