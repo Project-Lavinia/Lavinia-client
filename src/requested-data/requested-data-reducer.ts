9191617088d194cabf55d22db9f7b40315727594
Simen <simen.d@gmail.com>
2018-11-20 18:01:19 +0100

    docs: comment reducer

--- src/requested-data/requested-data-reducer.ts
+++ src/requested-data/requested-data-reducer.ts
@@ -1,6 +1,13 @@
 ﻿import { RequestedDataActionType, RequestedDataAction } from "./requested-data-actions";
 import { RequestedDataState, unloadedState } from "./requested-data-state";
 
+/**
+ *
+ * @param state - the current state, with default parameters iff it is
+ * undefined.
+ * @param action - the action to act upon the state.
+ * @returns a new state mutated by the action passed in as parameter.
+ */
 export function requestedData(
     state: RequestedDataState = unloadedState,
     action: RequestedDataAction
