864794b62a7c8f765aa3c778e872f43625a01055
Simen <simen.d@gmail.com>
2018-11-20 17:57:00 +0100

    docs: comment reducer

--- src/Layout/PresentationMenu/presentation-menu-reducer.ts
+++ src/Layout/PresentationMenu/presentation-menu-reducer.ts
@@ -2,6 +2,14 @@
 import { PresentationMenuActionType, PresentationMenuAction } from "./presentation-menu-actions";
 import { checkExhaustively } from "../../utilities";
 
+/**
+ * Reducer for the presentation menu. Handles all state changes to the presentation menu.
+ *
+ * @param state - the current state, with default parameters iff it is
+ * undefined.
+ * @param action - the action to act upon the state.
+ * @returns a new state mutated by the action passed in as parameter.
+ */
 export function presentationMenuReducer(
     state: PresentationMenuState = unloadedState,
     action: PresentationMenuAction
