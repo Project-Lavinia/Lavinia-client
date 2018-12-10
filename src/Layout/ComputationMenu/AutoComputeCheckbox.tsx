ebb12f2ef8d981203fa6f3408450834199768a75
Simen <8669774+simendag@users.noreply.github.com>
2018-12-10 14:12:18 +0100

    refactor: fix issue with manual computation button

--- src/Layout/ComputationMenu/AutoComputeCheckbox.tsx
+++ src/Layout/ComputationMenu/AutoComputeCheckbox.tsx
@@ -10,23 +10,35 @@ export interface AutoComputeCheckboxProps {
 export class AutoComputeCheckbox extends React.Component<AutoComputeCheckboxProps> {
     render() {
         return (
-            <div className="form-group row">
-                <label htmlFor="autoCompute" className="text-left col-sm-10 col-form-label">
-                    Oppdater automatisk
-                </label>
-                <div className="col-sm-2">
-                    <input
-                        className="form-control"
-                        type="checkbox"
-                        name="autoCompute"
-                        checked={this.props.autoCompute}
-                        onChange={this.props.toggleAutoCompute}
-                    />
-                    {!this.props.autoCompute && (
-                        <Button title={"Kalkuler"} onPress={this.props.computeManually} type="button" />
-                    )}
+            <React.Fragment>
+                <div className="form-group row">
+                    <label htmlFor="autoCompute" className="text-left col-sm-10 col-form-label">
+                        Oppdater automatisk
+                    </label>
+                    <div className="col-sm-2">
+                        <input
+                            className="form-control"
+                            type="checkbox"
+                            name="autoCompute"
+                            checked={this.props.autoCompute}
+                            onChange={this.props.toggleAutoCompute}
+                        />
+                    </div>
                 </div>
-            </div>
+                {!this.props.autoCompute ? (
+                    <div className="form-group row">
+                        <label className="text-left col-sm-6">Manuell beregning</label>
+                        <div className="btn-group-vertical col-sm-6">
+                            <Button
+                                id="compute_manually"
+                                title={"Beregn"}
+                                onPress={this.props.computeManually}
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                ) : null}
+            </React.Fragment>
         );
     }
 }
