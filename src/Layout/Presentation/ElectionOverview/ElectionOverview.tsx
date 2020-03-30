2febb64318d341bcfb0aeec4270ddd49c25746c0
oysor <osorby@gmx.com>
2020-03-30 22:07:21 +0200

    feat: improve filter readability by using math expressions

--- src/Layout/Presentation/ElectionOverview/ElectionOverview.tsx
+++ src/Layout/Presentation/ElectionOverview/ElectionOverview.tsx
@@ -95,8 +95,8 @@ export class ElectionOverview extends React.Component<ElectionOverviewProps, {}>
         }
         const allTrueFalseOptions = [
             { value: "all", title: "Alle" },
-            { value: "true", title: "Ja" },
-            { value: "false", title: "Nei" },
+            { value: "true", title: "Ja >" },
+            { value: "false", title: "Nei =" },
         ];
 
         const thresholdOptions = [
