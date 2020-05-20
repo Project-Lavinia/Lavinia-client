812180a478f744d662a1e20bd3c58b1f1344308e
oysor <osorby@gmx.com>
2020-05-20 14:41:27 +0200

    Add url prop to TooltipInfo

--- src/common/TooltipInfo/TooltipInfo.tsx
+++ src/common/TooltipInfo/TooltipInfo.tsx
@@ -2,14 +2,16 @@ import * as React from "react";
 
 export interface TooltipInfoProps {
     text: string;
+    url?: string;
 }
 
 export class TooltipInfo extends React.Component<TooltipInfoProps, {}> {
     render() {
         return (
             <span className="icon has-tooltip-multiline has-tooltip-arrow" data-tooltip={this.props.text}>
-                {" "}
+                <a href={this.props.url} target="_blank">
                 <i className="fas fa-info-circle" />
+                </a>
             </span>
         );
     }
