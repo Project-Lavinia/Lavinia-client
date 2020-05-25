d1f8b4b74118bc34214f027a3825685363c1f764
oysor <osorby@gmx.com>
2020-05-25 22:53:41 +0200

    Add bold text to buttons 'Beregn manuelt' and 'Tilbakestill Innstillinger'

--- src/Layout/ComputationMenu/ComputeManuallyButton.tsx
+++ src/Layout/ComputationMenu/ComputeManuallyButton.tsx
@@ -11,7 +11,7 @@ export class ComputeManuallyButton extends React.Component<ComputeManuallyButton
             <div hidden={this.props.autoCompute}>
                 <button
                     id="compute_manually"
-                    className="button is-primary is-outlined is-fullwidth"
+                    className="button is-primary is-outlined is-fullwidth has-text-weight-bold"
                     title={"Beregn manuelt"}
                     onClick={this.props.computeManually}
                     type="button"
