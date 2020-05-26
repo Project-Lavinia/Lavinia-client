a0e1a5680de20c6e9d78702909e16d05f6b946ea
oysor <osorby@gmx.com>
2020-05-26 22:54:46 +0200

    Add noreferrer noopener to TooltipInfo urls

--- src/common/TooltipInfo/TooltipInfo.tsx
+++ src/common/TooltipInfo/TooltipInfo.tsx
@@ -9,7 +9,7 @@ export class TooltipInfo extends React.Component<TooltipInfoProps, {}> {
     render() {
         return (
             <span className="icon has-tooltip-multiline has-tooltip-arrow" data-tooltip={this.props.text}>
-                <a href={this.props.url} target="_blank">
+                <a href={this.props.url} target="_blank" rel="noreferrer noopener">
                 <i className="fas fa-info-circle" />
                 </a>
             </span>
--- src/common/TooltipInfo/TooltipInfoRight.tsx
+++ src/common/TooltipInfo/TooltipInfoRight.tsx
@@ -5,7 +5,7 @@ export class TooltipInfoRight extends React.Component<TooltipInfoProps, {}> {
     render() {
         return (
             <span className="icon has-tooltip-multiline has-tooltip-arrow has-tooltip-right" data-tooltip={this.props.text}>
-                <a href={this.props.url} target="_blank">
+                <a href={this.props.url} target="_blank" rel="noreferrer noopener">
                 <i className="fas fa-info-circle" />
                 </a>
             </span>
