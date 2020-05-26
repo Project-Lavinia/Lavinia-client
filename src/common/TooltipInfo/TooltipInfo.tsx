0181da4650051a9e590ea94745c34dbe89f52d68
oysor <osorby@gmx.com>
2020-05-26 21:05:21 +0200

    Refactor ToolTipInfo

--- src/common/TooltipInfo/TooltipInfo.tsx
+++ src/common/TooltipInfo/TooltipInfo.tsx
@@ -17,14 +17,4 @@ export class TooltipInfo extends React.Component<TooltipInfoProps, {}> {
     }
 }
 
-export class TooltipInfoRight extends React.Component<TooltipInfoProps, {}> {
-    render() {
-        return (
-            <span className="icon has-tooltip-multiline has-tooltip-arrow has-tooltip-right" data-tooltip={this.props.text}>
-                <a href={this.props.url} target="_blank">
-                <i className="fas fa-info-circle" />
-                </a>
-            </span>
-        );
-    }
-}
+
---
+++ src/common/TooltipInfo/TooltipInfoRight.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { TooltipInfoProps } from "./TooltipInfo";
+
+export class TooltipInfoRight extends React.Component<TooltipInfoProps, {}> {
+    render() {
+        return (
+            <span className="icon has-tooltip-multiline has-tooltip-arrow has-tooltip-right" data-tooltip={this.props.text}>
+                <a href={this.props.url} target="_blank">
+                <i className="fas fa-info-circle" />
+                </a>
+            </span>
+        );
+    }
+}
