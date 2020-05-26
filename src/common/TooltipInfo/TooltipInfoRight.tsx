69040f2df01e9ae7dcf0fba01e9dcecad3bbc9a7
Anders Jakob Sivesind <ajsivesind@gmail.com>
2020-05-27 01:36:13 +0200

    fix: ensure all info symbols uses primary colour

--- src/common/TooltipInfo/TooltipInfoRight.tsx
+++ src/common/TooltipInfo/TooltipInfoRight.tsx
@@ -4,9 +4,12 @@ import { TooltipInfoProps } from "./TooltipInfo";
 export class TooltipInfoRight extends React.Component<TooltipInfoProps, {}> {
     render() {
         return (
-            <span className="icon has-tooltip-multiline has-tooltip-arrow has-tooltip-right" data-tooltip={this.props.text}>
+            <span
+                className="icon has-tooltip-multiline has-tooltip-arrow has-tooltip-right"
+                data-tooltip={this.props.text}
+            >
                 <a href={this.props.url} target="_blank" rel="noreferrer noopener">
-                <i className="fas fa-info-circle" />
+                    <i className="fas fa-info-circle has-text-primary" />
                 </a>
             </span>
         );
