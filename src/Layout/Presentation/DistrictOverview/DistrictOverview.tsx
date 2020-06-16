54135c4afd4d5682dd279411d30fa6c03c450fce
oysor <osorby@gmx.com>
2020-06-16 23:56:30 +0200

    Remove minWidth in first column DistrictOverview

--- src/Layout/Presentation/DistrictOverview/DistrictOverview.tsx
+++ src/Layout/Presentation/DistrictOverview/DistrictOverview.tsx
@@ -65,7 +65,6 @@ export class DistrictOverview extends React.Component<DistrictOverviewProps, {}>
                         {
                             Header: <span className="is-pulled-left"> {"Fylke"}</span> ,
                             accessor: "name",
-                            minWidth: this.props.districtWidth * 10,
                             Cell: (row) => {
                                 return <span className="is-pulled-left" >{row.value}</span>
                             },
