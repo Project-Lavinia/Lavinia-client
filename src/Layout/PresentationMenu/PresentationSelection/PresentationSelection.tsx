1f156906d9c0dcba26273c7e379e9709f876d096
oysor <osorby@gmx.com>
2020-09-01 13:39:32 +0200

    Address Codacy issues

--- src/Layout/PresentationMenu/PresentationSelection/PresentationSelection.tsx
+++ src/Layout/PresentationMenu/PresentationSelection/PresentationSelection.tsx
@@ -30,37 +30,37 @@ export class PresentationSelection extends React.Component<PresentationSelection
 
     selectTooltipContent(table: string | undefined) {
         switch (table) {
-            case 'TABLE_ELECTION_OVERVIEW': {
+            case "TABLE_ELECTION_OVERVIEW": {
                 return {
                     text: "Her kan du se valgresultatet på landsbasis. Trykk på meg for mer informasjon!",
                     url: "#Landsoversikt"
                 };
             }
-            case 'TABLE_DISTRICT_OVERVIEW': {
+            case "TABLE_DISTRICT_OVERVIEW": {
                 return {
                     text: "Her kan du se valgresultatet for hvert fylke.",
                     url: "#Fylkesoversikt"
                 };
             }
-            case 'TABLE_LEVELLING_SEATS_OVERVIEW': {
+            case "TABLE_LEVELLING_SEATS_OVERVIEW": {
                 return {
                     text: "Her kan du se fra hvilke fylker partiene har fått utjevningsmandater fra.",
                     url: "#Utjevningsmandater."
                 };
             }
-            case 'TABLE_REMAINDER_QUOTIENTS': {
+            case "TABLE_REMAINDER_QUOTIENTS": {
                 return {
                     text: "Her ser du en oversikt over restkvotienter.",
                     url: "#Restkvotienter"
                 };
             }
-            case 'SEAT_DISTRIBUTION': {
+            case "SEAT_DISTRIBUTION": {
                 return {
                     text: "Her ser du fordelingen av mandater på hvert fylke.",
                     url: "#Fylkesfordeling%20av%20mandater"
                 };
             }
-            case 'TABLE_SINGLE_COUNTY': {
+            case "TABLE_SINGLE_COUNTY": {
                 return {
                     text: "Her kan du velge fylke og se en fullstendig oversikt for valgt fylke.",
                     url: "#Enkeltfylke"
@@ -70,7 +70,7 @@ export class PresentationSelection extends React.Component<PresentationSelection
                 return {
                     text: "Trykk på ikonet for å lese om oversikten.",
                     url: "#Visning"
-                };;
+                };
             }
         }
     }
